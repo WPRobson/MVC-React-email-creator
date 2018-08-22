7a9adda29b054e1677b13d74c49e700c875cedba
Will Robson <wrobson2244@gmail.com>
2018-08-22 17:32:34 +0100

    Clean up

---
+++ MVC_React/ClientApp/components/Email Builder/Data Builder/Contacts.tsx
@@ -0,0 +1,36 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import { Link, NavLink } from 'react-router-dom';
+
+
+declare var Dropzone: any;
+declare var fileDropZone: any;
+
+export class Contacts extends React.Component<any, any> {
+    constructor() {
+        super();
+
+
+        this.state = {
+
+
+        };
+
+
+    }
+
+
+    public render() {
+        return <div style={{ height: '100%' }}>
+            <p>contacts</p>
+
+
+            <form action="/file-upload"
+                className="dropzone"
+                id="my-awesome-dropzone"></form>
+
+
+        </div>;
+    }
+
+}
--- MVC_React/ClientApp/components/Email Builder/EmailCreator.tsx
+++ MVC_React/ClientApp/components/Email Builder/EmailCreator.tsx
@@ -1,7 +1,8 @@
 ﻿import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { Link, NavLink } from 'react-router-dom';
-import { HtmlBuilder} from './HTML Editor/HtmlBuilder'
+import { HtmlBuilder } from './HTML Editor/HtmlBuilder'
+import { Contacts } from './Data Builder/Contacts'
 
 
 
@@ -9,7 +10,7 @@ export class EmailCreator extends React.Component<RouteComponentProps<{ id: any 
     constructor() {
         super();
         this.state = {
-           
+
 
         };
 
@@ -20,34 +21,12 @@ export class EmailCreator extends React.Component<RouteComponentProps<{ id: any 
     public render() {
         return <div className='' style={{ height: '100%' }}>
 
-            
-            <ul className="nav nav-tabs">
-                <li className="nav-item active">
-                    <a data-toggle="tab" className="nav-link active" href="#EmailEditor" aria-expanded="true" aria-selected="true">Email Content</a>
-                </li>
-
-                <li className="nav-item">
-                    <a data-toggle="tab" className="nav-link" href="#DataEditor">Data</a>
-                </li>
-            </ul>
-
-
-            <div className="tab-content" style={{ height: '100%' }}>
-                <div id="EmailEditor" className="tab-pane active" style={{ height: '100%' }}>
-                    <HtmlBuilder />
-                </div>
-                <div id="DataEditor" className="tab-pane" style={{ height: '100%' }}>
-                    <p>data</p>
-                </div>
-
-
-
+            <div style={{ height: '100%' }}>
+                <HtmlBuilder />
             </div>
 
+            <p>Id: {this.props.match.params.id}</p>
 
-
-            <p>Id: {this.props.match.params.id}</p> 
-           
 
         </div>;
     }
